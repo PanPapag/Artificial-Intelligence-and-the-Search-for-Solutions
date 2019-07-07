dacabae897d54d7529d4a3435187ec3cacb79a85
PanPapag <pan_papageorgiou@hotmail.com>
2019-07-07 16:48:09 +0300

    countries of south america added

---
+++ Map_Coloring/data.pl
@@ -0,0 +1,16 @@
+% Geographical data for South America to be used with cmap.pl
+
+country(antilles),
+country(bolivia),
+country(columbia),
+country(ecuador),
+country(guyana),
+country(peru),
+country(uruguay),
+country(argentina),
+country(brazil),
+country(chile),
+country(french_guiana),
+country(paraguay),
+country(surinam),
+country(venezuela).
