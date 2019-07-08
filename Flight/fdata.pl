35eb124043d8bd0cf8df150f32ea06d0f0d8f56e
PanPapag <pan_papageorgiou@hotmail.com>
2019-07-08 18:08:23 +0300

    fligth data description added

---
+++ Flight/fdata.pl
@@ -0,0 +1,7 @@
+%􏰠 􏰠fdata.pl
+% 􏰠 􏰠
+% Travel data for Acme Airlines􏰍
+% The first two arguments in each clause list a pair of cities
+% served by Acme, the third argument gives the distance between
+% these cities􏰋 and the fourth tells wether Acme has direct
+% flights between these cities􏰍
